427575c3326a9f51108a5c7b78929d32f279cd17
Jonathan Ng <jonjon.ngng@gmail.com>
2020-09-14 23:41:45 -0400

    Slow printing text

--- utilities.js
+++ utilities.js
@@ -2,15 +2,17 @@ const prompt = async (text) => {
     write(text, '\r\n');
     return await require("readline-async")();
 };
-const write = (text) => {
+const write = async (text) => {
     return new Promise(async (resolve, reject) => {
+        let position = 0;
 
-        for (let position = 0; position < text.length; ++position) {
-            await setTimeout(() => {
-                process.stdout.write(text[position]);
-            }, 500);
+        while (position < text.length) {
+            let p = position;
+            setTimeout(() => {
+                process.stdout.write(text[p]);
+            }, 50 * p);
+            position++;
         }
-
         resolve();
     });
 };
